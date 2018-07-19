fbde9db30b1f0c8b1b6369155b65242ffef7f4cf
Jennifer Pazos <jmp521@gmail.com>
2018-07-19 13:30:39 -0500

    added the tbody, column and row tags

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,11 +1,18 @@
-
+import React from 'react'
 const Quote = (props) =>  {
-  <div className="quote">
-    {props.source}
-    <p>{props.context}</p>
-    
 
-  </div>
+  return(
+
+    <tbody key={props.k}>
+      <tr key={props.k}>
+        <td style={{width: 600+'px'}} key={props.k}>{props.quotes.quote}</td>
+        <td key={props.k}>{props.quotes.context}</td>
+        <td key={props.k}>{props.quotes.source}</td>
+        <td key={props.k}>{props.quotes.theme}</td>
+      </tr>
+    </tbody>
+
+  )
 }
 
 export default Quote
