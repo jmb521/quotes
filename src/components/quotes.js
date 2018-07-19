18e0474eebc1a7f73078f882e6f3b7082eadc3c5
Jennifer Pazos <jmp521@gmail.com>
2018-07-19 14:50:53 -0500

    added functions to update state after fetching quotes, added pagination

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,24 +1,180 @@
 import React, {Component} from 'react'
-
+import Quote from './quote.js'
+import Search from './search.js'
+import Filter from './filter.js'
 class Quotes extends Component {
+  constructor() {
+    super()
 
-  fetchQuotes = () => {
-    fetch('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/b191cf3b6ea9cdcca8b363516ff969261398061f/quotes.json')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-      });
+    this.state = {
+      allquotes: [],
+      quotes: [],
+      search: "",
+      currentPage: 1,
+      quotesPerPage: 15,
+    }
   }
   componentDidMount() {
     this.fetchQuotes()
+    this.allQuotes()
+  }
+  setQuote = (data) => {
+    this.setState({
+      allquotes: data,
+    })
+  }
+
+  fetchQuotes = () => {
+    return fetch('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/b191cf3b6ea9cdcca8b363516ff969261398061f/quotes.json')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => this.setQuote(data))
+
+  }
+
+  submitForm = (e) =>  {
+    e.preventDefault()
+    this.searchQuotes()
+
+  }
+
+  searchQuotes = () => {
+    const filtered = this.state.allquotes.filter((q) => {
+      // console.log(q.quote)
+      // console.log(this.state.search)
+      // debugger
+      if(q.quote.includes(this.state.search)) {
+        return q
+      }
+      else {
+        return ""
+      }
+    })
+    this.setState({
+      quotes: filtered,
+      currentPage: 1,
+    })
+    return filtered
+  }
+  // updateSearchState = (match) => {
+  //   this.setState({
+  //     quotes: match
+  //   })
+  // }
+
+
+  handleOnChange = (event) => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  filterQuotes = (e) => {
+
+    if(e.target.value === "") {
+      this.setState({
+        quotes: this.state.allquotes
+      })
+    }
+    if (e.target.value === "games"){
+      this.setState({
+        quotes: this.state.allquotes.filter(f => f.theme === "games"),
+        currentPage: 1,
+      })
+    }
+    if (e.target.value === "movies") {
+      this.setState({
+        quotes: this.state.allquotes.filter(f => f.theme === "movies"),
+        currentPage: 1,
+      })
+
+    }
+
+  }
+
+
+    handleClick = (event) => {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+
+  }
+  // componentDidUpdate(prevProps) {
+  //   if("game"!== prevProps.theme) {
+  //     console.log("pp", prevProps)
+  //   }
+  // }
+  allQuotes = () =>  {
+    this.setState({
+      quotes: this.state.allquotes
+    })
   }
 
   render() {
+    const Showquotes = () => {
+      const indexOfLastQuote = this.state.currentPage * this.state.quotesPerPage
+      const indexOfFirstQuote = indexOfLastQuote - this.state.quotesPerPage
+
+      const currentQuotes = this.state.quotes.slice(indexOfFirstQuote, indexOfLastQuote);
+      return currentQuotes.map((f, i) =>{
+        return <Quote quotes={f} key={i} />
+      })
+    }
+
+
+
+
+
+
+  //   const renderTodos = currentTodos.map((todo, index) => {
+  //    return <li key={index}>{todo}</li>;
+  //  });
+   //
+  //  // Logic for displaying page numbers
+   const pageNumbers = [];
+   for (let i = 1; i <= Math.ceil(this.state.quotes.length / this.state.quotesPerPage); i++) {
+     pageNumbers.push(i);
+   }
+   //
+   const renderPageNumbers = pageNumbers.map(number => {
+     return (
+       <li
+         key={number}
+         id={number}
+         onClick={this.handleClick}
+       >
+         {number}
+       </li>
+     );
+   });
+
+
+
+
     return(
       <div>
-        
+      <br />
+      <Search submitForm={this.submitForm} handleOnChange={this.handleOnChange} value={this.state.search}/>
+      <Filter filterquote={this.filterQuotes}/>
+      <br />
+      <br />
+        <table>
+        <thead>
+        <tr>
+
+          <th>Quote</th>
+          <th>Context</th>
+          <th>Source</th>
+          <th>Theme</th>
+
+          </tr>
+          </thead>
+        <Showquotes />
+        </table>
+        <ul id="page-numbers">
+        {renderPageNumbers}
+        </ul>
       </div>
     )
   }
